a1890b7a85439f94e84e733b2e361cf83dd9cb3d
Matheus Bersot <matheusbersot@id.uff.br>
2015-09-06 18:46:33 -0300

    CLOSES #1 por Matheus Bersot: Criar tabela de banco de dados

--- www/js/app.js
+++ www/js/app.js
@@ -1,11 +1,16 @@
 // Ionic Starter App
 
+
+// Inicializando módulos
+angular.module('myApp.config', []);
+angular.module('myApp.services', []);
+
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic','ui.router', 'myApp.config', 'myApp.services'])
 
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform, DB, $state) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -15,5 +20,11 @@ angular.module('starter', ['ionic'])
     if(window.StatusBar) {
       StatusBar.styleDefault();
     }
+
+    //Inicializando o banco de dados
+    DB.init().then(function(valor)
+    {
+      $state.go("home");
+    });
   });
 })
---
+++ www/js/config/config.js
@@ -0,0 +1,45 @@
+var modConfig = angular.module('myApp.config');
+
+modConfig.constant('DB_CONFIG', {
+  name: 'DB',
+  tables: [
+    {
+      name: 'agencia',
+      columns: [
+        {name: 'id', type: 'integer primary key'},
+        {name: 'nome', type: 'text'},
+        {name: 'url', type: 'text'}
+      ]
+    },
+    {
+      name: 'moeda',
+      columns: [
+        {name: 'id', type: 'integer primary key'},
+        {name: 'nome', type: 'text'},
+        {name: 'sigla', type: 'text'}
+      ]
+    },
+    {
+      name: 'agencia_moeda',
+      columns: [
+        {name: 'id', type: 'integer primary key'},
+        {name: 'id_agencia', type: 'integer'},
+        {name: 'id_moeda', type: 'integer'},
+        {name: 'valor', type: 'float'},
+        {name: 'diffUltimaAtualizacao', type: 'integer'}, /* em segundos */
+        {name: 'foreign key(id_agencia)', type: 'references agencia(id)'},
+        {name: 'foreign key(id_moeda)', type: 'references moeda(id)'}
+      ]
+    }
+  ]
+});
+
+modConfig.config(function ($stateProvider) {
+
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: 'index.html',
+      cache: false
+    })
+});
